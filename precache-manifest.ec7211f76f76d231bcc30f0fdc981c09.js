23489d0d650a9aec66afc74097915ff67559d814
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-10-20 19:16:11 +0900

    Updates

---
+++ precache-manifest.ec7211f76f76d231bcc30f0fdc981c09.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbc0adc14067dff3893ea34e943a6ed",
+    "url": "/ra4w1_hmi_demo_app/index.html"
+  },
+  {
+    "revision": "ab0f2f0cf3f4f63a3872",
+    "url": "/ra4w1_hmi_demo_app/static/js/2.7e0eeafe.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ra4w1_hmi_demo_app/static/js/2.7e0eeafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3774d010cdad1798de",
+    "url": "/ra4w1_hmi_demo_app/static/js/main.ce39d152.chunk.js"
+  },
+  {
+    "revision": "0bf0a7729814a80697c0",
+    "url": "/ra4w1_hmi_demo_app/static/js/runtime-main.7d09c4aa.js"
+  },
+  {
+    "revision": "b8a4479e9d92aa0b87ec0b414b2aadde",
+    "url": "/ra4w1_hmi_demo_app/static/media/connect.b8a4479e.png"
+  },
+  {
+    "revision": "be44afbcb256e256dc0504537d64fbad",
+    "url": "/ra4w1_hmi_demo_app/static/media/electric_fun_stop.be44afbc.png"
+  },
+  {
+    "revision": "451108d270d7170c338f87e93715b713",
+    "url": "/ra4w1_hmi_demo_app/static/media/electric_fun_swing.451108d2.gif"
+  },
+  {
+    "revision": "3588c63660f9abd78fad0483f53183b6",
+    "url": "/ra4w1_hmi_demo_app/static/media/indoor_environment.3588c636.png"
+  },
+  {
+    "revision": "6f62af6329c85620f351ef06972c0cae",
+    "url": "/ra4w1_hmi_demo_app/static/media/led_control.6f62af63.png"
+  },
+  {
+    "revision": "4b6fe806817ed3f30bc11bcca21a9819",
+    "url": "/ra4w1_hmi_demo_app/static/media/vr_result.4b6fe806.png"
+  },
+  {
+    "revision": "ef163af4b5972ba1b7b4662dcff28fb7",
+    "url": "/ra4w1_hmi_demo_app/static/media/vr_settings.ef163af4.png"
+  }
+]);
